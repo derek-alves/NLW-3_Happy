767fc38693a8806561048ab225b412deb69e0de5
derek-alves <derek.alves@fatec.sp.gov.br>
2020-10-14 23:53:43 -0300

    Server done

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,11 +1,18 @@
 import {Router} from 'express';
+import multer from 'multer';
+
+import uploadConfig from './config/upload';
+
 import orphanagesController from '../src/controllers/OrphanagesController';
 
 
 const routes = Router();
+const upload = multer(uploadConfig);
 
-routes.post('/orphanages', orphanagesController.create);
+routes.post('/orphanages',upload.array('images'), orphanagesController.create);
 routes.get('/orphanages', orphanagesController.index);
-routes.get('/orphanages/:id', orphanagesController.show);
+routes.get('/orphanages/:id',orphanagesController.show);
 
 export default routes;
+
+
